8ec499dce1b8f78ca9f72f2279df377117488261
MATTTTJ <hoon3022@naver.com>
2024-03-12 16:58:53 +0900

    clear 2309

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -4,34 +4,33 @@
 #include <string>
 using namespace std;
 
-void printV(vector<int> &v)
-{
-	for(int i = 0; i < v.size(); i++)
-	{
-		cout << v[i] << " ";
-	}
-	cout << "\n";
-}
+int a[9];
+
 int main()
 {
-	int a[3] = { 1,2,3 };
-	vector<int> v;
+	for(int i = 0; i < 9; i++)
+	{
+		cin >> a[i];
+	}
 
-	for (int i = 0; i < 3; i++)
-		v.push_back(a[i]);
+	sort(a,a+9);
+
 	do
 	{
-		printV(v);
-	} while (next_permutation(v.begin(), v.end()));
-	cout << "--------------" << '\n';
-	v.clear();
-	for (int i = 2; i >= 0; i--)
-		v.push_back(a[i]);
-	do
-	{
-		printV(v);
-	} while (prev_permutation(v.begin(), v.end()));
+		int sum = 0;
+		for(int i = 0; i < 7; i++)
+		{
+			sum += a[i];
+		}
+		if (sum == 100)
+			break;
+	} while (next_permutation(a, a + 9));
 
+	for(int i = 0; i < 7; i++)
+	{
+		cout << a[i] << "\n";
+	}
+	
 	return 0;
 }
 
