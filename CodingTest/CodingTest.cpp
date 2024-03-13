f17105beb271f13796bf9866ec36bf59ba76dbda
MATTTTJ <hoon3022@naver.com>
2024-03-13 17:15:29 +0900

    clear 1620, endl 대신 \n 을 사용하는게 그렇게 큰 성능 향상의 이득을 취할 수 있나? 조금 의구심이 듦

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,27 +1,42 @@
 #include <algorithm>
 #include <iostream>
+#include <map>
 #include <vector>
 #include <string>
 using namespace std;
 
-int n, k, temp, psum[100001], ret = -1000000;
-
+int n, m;
+string s;
+map<string, int> mp;
+map<int, string> mp2;
+vector<string> str;
 int main()
 {
-	cin >> n >> k;
+	cin >> n >> m;
 
-	for(int i = 1; i <= n; i++)
+	for(int i = 0; i < n; i++)
 	{
-		cin >> temp;
-		psum[i] = psum[i - 1] + temp;
+		cin >> s;
+		mp[s] = i + 1;
+		mp2[i + 1] = s;
 	}
 
-	for(int i = k; i <= n; i++)
+	for(int i = 0; i < m; i++)
 	{
-		ret = max(ret, psum[i] - psum[i - k]);
+		cin >> s;
+		str.push_back(s);
 	}
 
-	cout << ret << "\n";
+	for(int i = 0; i < (int)str.size(); i++)
+	{
+		if (atoi(str[i].c_str()) == 0)
+		{
+			cout << mp[str[i]] << "\n";
+		}
+		else
+			cout << mp2[atoi(str[i].c_str())] << "\n";
+	}
+
 
 	return 0;
 }
