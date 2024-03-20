1df40889e3fc55e6ad660f5df978b4563f55a91c
MATTTTJ <hoon3022@naver.com>
2024-03-20 18:14:44 +0900

    clear 2178 BFS

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,36 +1,99 @@
-#include <algorithm>
 #include <iostream>
-#include <map>
-#include <stack>
-#include <vector>
-#include <string>
+#pragma warning(disable:4996)
+#include <queue>
+#include <tuple>
+
 using namespace std;
 
-typedef long long ll;
+const int max_n = 104;
 
-int n;
+int n, m, a[max_n][max_n], visited[max_n][max_n], y, x;
+
+int dy[4] = { -1, 0, 1, 0 };
+int dx[4] = { 0, 1, 0, -1 };
 
 int main()
 {
-	while(scanf_s("%d", &n) != EOF)
+	scanf("%d %d", &n, &m);
+
+	for(int i = 0; i < n; i++)
 	{
-		int cnt = 1, ret = 1;
-		while(true)
+		for(int j = 0; j < m; j++)
 		{
-			if(cnt % n == 0)
-			{
-				printf("%d\n", ret);
-				break;
-			}
-			else
-			{
-				cnt = (cnt * 10) + 1;
-				cnt %= n;
-				ret++;
-			}
+			scanf("%1d", &a[i][j]);
 		}
 	}
+
+	queue<pair<int, int>> q;
+	visited[0][0] = 1;
+	q.push({ 0,0 });
+
+	while(q.size())
+	{
+		tie(y, x) = q.front();
+		q.pop();
+
+		for(int i = 0; i < 4; i++)
+		{
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+
+			if(ny < 0 || nx < 0 || ny >= n || nx >= m || a[ny][nx] == 0)
+				continue;
+			if(visited[ny][nx] >= 1)
+				continue;
+
+			visited[ny][nx] = visited[y][x] + 1;
+			q.push({ ny, nx });
+		}
+	}
+
+	printf("%d", visited[n - 1][m - 1]);
 
 	return 0;
 }
 
+/*
+ * using namespace std;
+
+const int max_n = 104;
+int dy[4] = { -1, 0, 1, 0 }, dx[4] = { 0, 1, 0, -1 };
+int n, m, a[max_n][max_n], visited[max_n][max_n], y, x;
+
+int main()
+{
+	scanf("%d %d", &n, &m);
+
+	for(int i =0; i< n; i++)
+	{
+		for(int j = 0; j <m; j++)
+		{
+			scanf("%1d", &a[i][j]);
+		}
+	}
+
+	queue<pair<int, int>> q;
+	visited[0][0] = 1;
+	q.push({ 0, 0 });
+
+	while(q.size())
+	{
+		tie(y, x) = q.front();
+		q.pop();
+
+		for(int i =0; i < 4; i++)
+		{
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+
+			if (ny < 0 || ny >= n || nx < 0 || nx >= m || a[ny][nx] == 0) continue;
+			if (visited[ny][nx])  continue;
+			visited[ny][nx] = visited[y][x] + 1;
+			q.push({ ny, nx });
+		}
+	}
+
+	printf("%d", visited[n - 1][m - 1]);
+	return 0;
+}
+ */
