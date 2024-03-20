6f56981f2303a06a8ef61f9e25f03f5dcefe268c
MATTTTJ <hoon3022@naver.com>
2024-03-20 19:22:07 +0900

    clear 2468 DFS

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,19 +1,20 @@
-#include <iostream>
 #pragma warning(disable:4996)
+
+#include <iostream>
 #include <queue>
 #include <tuple>
 #include <vector>
 using namespace std;
 
-const int max_cnt = 51;
-int m, n, k, y, x, ret, ny, nx, t;
-int a[max_cnt][max_cnt];
-bool visited[max_cnt][max_cnt];
+const int max_n = 101;
+int n, ny, nx, d, ret = 1;
+int a[max_n][max_n];
+bool visited[max_n][max_n];
 
 int dy[4] = { -1, 0, 1, 0 };
 int dx[4] = { 0, 1, 0, -1 };
 
-void dfs(int y, int x)
+void dfs(int y, int x, int d)
 {
 	visited[y][x] = true;
 
@@ -22,44 +23,46 @@ void dfs(int y, int x)
 		ny = y + dy[i];
 		nx = x + dx[i];
 
-		if (ny < 0 || nx < 0 || ny >= n || nx >= m)
+		if (ny < 0 || nx < 0 || ny >= n || nx >= n)
 			continue;
-		if (a[ny][nx] == 1 && !visited[ny][nx])
-			dfs(ny, nx);
+		if (a[ny][nx] > d && !visited[ny][nx])
+			dfs(ny, nx, d);
 	}
 }
 
 int main()
 {
-	cin >> t;
+	cin >> n;
 
-	while (t--)
+	for (int i = 0; i < n; i++)
 	{
-		fill(&a[0][0], &a[0][0] + max_cnt * max_cnt, 0);
-		fill(&visited[0][0], &visited[0][0] + max_cnt * max_cnt, 0);
-		ret = 0;
-		cin >> m >> n >> k;
-
-		for (int i = 0; i < k; i++)
+		for (int j = 0; j < n; j++)
 		{
-			cin >> x >> y;
-			a[y][x] = 1;
+			cin >> a[i][j];
 		}
+	}
 
+
+	for (d = 1; d < 101; d++)
+	{
+		fill(&visited[0][0], &visited[0][0] + max_n * max_n, 0);
+		int cnt = 0;
 
 		for (int i = 0; i < n; i++)
 		{
-			for (int j = 0; j < m; j++)
+			for (int j = 0; j < n; j++)
 			{
-				if (a[i][j] == 1 && !visited[i][j])
+				if (a[i][j] > d && !visited[i][j])
 				{
-					dfs(i, j);
-					ret++;
+					dfs(i, j, d);
+					cnt++;
 				}
 			}
 		}
-
-		cout << ret << "\n";
+		ret = max(ret, cnt);
 	}
+
+	cout << ret << endl;
+
 	return 0;
 }
