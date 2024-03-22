0c7c58ae00ba047f8f16eb858eafd6abcae4b2f7
MATTTTJ <hoon3022@naver.com>
2024-03-22 14:27:11 +0900

    clear 2828 greedy

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -6,45 +6,36 @@
 #include <algorithm>
 using namespace std;
 
-int n;
-string s;
-char a[101][101];
-string quard(int y, int x, int size)
-{
-	if (size == 1) return string(1, a[y][x]);
-	char b = a[y][x];
-	string ret = "";
-	for(int i = y; i < y + size; i++)
-	{
-		for(int j = x; j < x + size; j++)
-		{
-			if( b!= a[i][j])
-			{
-				ret += '(';
-				ret += quard(y, x, size / 2);
-				ret += quard(y, x + size / 2, size / 2);
-				ret += quard(y + size / 2, x , size / 2);
-				ret += quard(y + size / 2, x + size / 2, size / 2);
-				ret += ')';
-				return ret;
-			}
-		}
-	}
-	return string(1, a[y][x]);
-}
+int n, m, j, l, r, temp, ret;
 
 int main()
 {
-	cin >> n;
-	for(int i =0; i < n; i++)
+	cin >> n >> m >> j;
+	l = 1;
+	for(int i =0 ; i < j ; i++)
 	{
-		cin >> s;
-		for(int j = 0; j < n; j++)
+		r = l + m - 1;
+		cin >> temp;
+
+		if(temp >= l && temp <= r)
 		{
-			a[i][j] = s[j];
+			continue;
+		}
+		else
+		{
+			if(temp < l)
+			{
+				ret += (l - temp);
+				l = temp;
+			}
+			else
+			{
+				l += (temp - r);
+				ret += (temp - r);
+			}
 		}
 	}
-	cout << quard(0, 0, n) << '\n';
+	cout << ret << "\n";
 
 	return 0;
 }
