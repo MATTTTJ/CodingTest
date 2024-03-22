73ff5d27454fe5e38c5b4b58635aafcc3450d17f
MATTTTJ <hoon3022@naver.com>
2024-03-22 15:01:56 +0900

    clear 2910 빈도정렬

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -4,38 +4,52 @@
 #include <queue>
 #include <vector>
 #include <algorithm>
+#include <map>
 using namespace std;
 
-int n, m, j, l, r, temp, ret;
+// 빈도정령 -> Custom Operator 생각나야함
+// 1순위로 정렬 -> 많은 수 (카운팅)
+// 2순위로 정렬 -> 먼저 나온 순위
+
+typedef long long ll;
+const ll INF = 1e18;
+int n, c, a[1004];
+vector<pair<int, int>> v;
+map<int, int> mp, mp_first;
+
+bool cmp(pair<int, int> a, pair<int,int> b)
+{
+	if(a.first == b.first)
+	{
+		return mp_first[a.second] < mp_first[b.second];
+	}
+
+	return a.first > b.first;
+}
 
 int main()
 {
-	cin >> n >> m >> j;
-	l = 1;
-	for(int i =0 ; i < j ; i++)
-	{
-		r = l + m - 1;
-		cin >> temp;
+	cin >> n >> c;
 
-		if(temp >= l && temp <= r)
+	for(int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+		mp[a[i]]++;
+		if (mp_first[a[i]] == 0)
+			mp_first[a[i]] = i + 1;
+	}
+	for(auto it : mp)
+	{
+		v.push_back({ it.second, it.first });
+	}
+	sort(v.begin(), v.end(), cmp);
+	for(auto i : v)
+	{
+		for(int j =0; j < i.first; j++)
 		{
-			continue;
-		}
-		else
-		{
-			if(temp < l)
-			{
-				ret += (l - temp);
-				l = temp;
-			}
-			else
-			{
-				l += (temp - r);
-				ret += (temp - r);
-			}
+			cout << i.second << " ";
 		}
 	}
-	cout << ret << "\n";
 
 	return 0;
 }
