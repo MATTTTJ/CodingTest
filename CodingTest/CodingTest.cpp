4562a6bf9f1e0ec1a52b06ea4ec971dd6be3f3ed
MATTTTJ <hoon3022@naver.com>
2024-03-25 18:12:27 +0900

    clear 9012 타입 : 자료구조, 스택, 문자열

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -5,29 +5,49 @@
 #include <vector>
 #include <algorithm>
 #include <map>
+#include <stack>
 #include <string>
 
 using namespace std;
 
-int n;
+int n, l, r;
+string s;
+
+bool check(string s)
+{
+	stack<char> stk;
+
+	for(char c : s)
+	{
+		if (c == '(')
+		{
+			stk.push(c);
+		}
+		else
+		{
+			if (!stk.empty())
+				stk.pop();
+			else
+				return false;
+		}
+	}
+	// 전부 pop 되어야만 true 반환
+	return stk.empty();
+}
 
 int main()
 {
 	cin >> n;
 
-	int i = 666;
-
-	for(;; i++)
+	for(int i = 0; i < n; i++)
 	{
-		if(to_string(i).find("666") != string::npos)
-		{
-			n--;
-		}
-		if (n == 0)
-			break;
-	}
+		cin >> s;
 
-	cout << i << "\n";
+		if (check(s))
+			cout << "YES" << "\n";
+		else
+			cout << "NO" << "\n";
+	}
 
 	return 0;
 }
