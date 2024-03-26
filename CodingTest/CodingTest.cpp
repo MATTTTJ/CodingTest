f7007401ca6249866a10297c7d5a8b0b7188c7e3
MATTTTJ <hoon3022@naver.com>
2024-03-26 22:41:18 +0900

    clear 15686 type : 백트래킹, 조합, 브루트포스

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -10,32 +10,74 @@
 #include <cstring>
 
 using namespace std;
+int n, m, a[51][51], visited[51][51], result = 987654321;
 
-int n, a[1000004], ret[1000004];
-stack<int> s;
+vector<pair<int,int>> house;
+vector<pair<int, int>> chicken;
+vector<vector<int>> chickenList;
+
+
+
+void combi(int start, vector<int> v)
+{
+	if(v.size() == m)
+	{
+		chickenList.push_back(v);
+		return;
+	}
+
+	for(int i = start + 1; i < chicken.size(); i++)
+	{
+		v.push_back(i);
+		combi(i, v);
+		v.pop_back();
+	}
+	return;
+}
+
 
 int main()
 {
-	cin >> n;
-	memset(ret, -1, sizeof(ret));
+	cin >> n >> m;
 
-	for(int i = 0; i < n; i ++)
+	for(int i = 0; i < n; i++)
 	{
-		cin >> a[i];
-
-		// 오큰수가 발생되는 순간
-		while(s.size() && a[s.top()] < a[i])
+		for (int j = 0; j < n; j++)
 		{
-			// 결과값을 담는다.
-			ret[s.top()] = a[i];
-			s.pop();
+			cin >> a[i][j];
+
+			if (a[i][j] == 1)
+			{
+				house.push_back({ i, j });
+			}
+			if (a[i][j] == 2)
+			{
+				chicken.push_back({ i,j });
+			}
 		}
-		// 오큰수가 결정이 안됐으면 일단 담아놓는다. 
-		s.push(i);
 	}
 
-	for (int i = 0; i < n; i++)
-		cout << ret[i] << " ";
+	vector<int> v;
+	combi(-1, v);
+	for(vector<int> cList : chickenList)
+	{
+		int ret = 0;
+		for(pair<int, int> home : house)
+		{
+			int _min = 987654321;
+			for(int ch : cList)
+			{
+				int _dist = abs(home.first - chicken[ch].first) + abs(home.second - chicken[ch].second);
+
+				_min = min(_min, _dist);
+			}
+			ret += _min;
+		}
+		result = min(result, ret);
+	}
+	cout << result << "\n";
 
 	return 0;
 }
+
+
