bad69c12e217982dd594c5116a6d6d6710f24034
MATTTTJ <hoon3022@naver.com>
2024-03-29 01:23:33 +0900

    clear 17427 type : Math

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -8,71 +8,17 @@
 
 using namespace std;
 
-const int dy[] = { -1, 0, 1, 0 };
-const int dx[] = { 0, 1, 0, -1 };
- 
-int n, m, visited[54][54], mx;
-char a[54][54];
-
-void bfs(int y, int x)
-{
-	memset(visited, 0, sizeof(visited));
-	visited[y][x] = 1;
-
-	queue<pair<int, int>> q;
-	q.push({ y,x });
-	while(!q.empty())
-	{
-		tie(y, x) = q.front();
-		q.pop();
-
-		for(int i = 0; i < 4; i++)
-		{
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-
-			if(ny < 0 || nx < 0 || ny >= n || nx >= m)
-				continue;
-			if(visited[ny][nx])
-				continue;
-			 if(a[ny][nx] == 'W')
-				 continue;
-			// 가중치 쌓기
-			 visited[ny][nx] = visited[y][x] + 1;
-			 q.push({ ny, nx });
-			 mx = max(mx, visited[ny][nx]);
-		}
-	}
-
-	return;  
-}
-
-
+int n;
+long long sum;
 int main()
 {
-	cin >> n >> m;
+	cin >> n;
 
-	for(int i = 0; i< n; i++)
+	for(int i = 1; i <= n; i++)
 	{
-		for(int j = 0; j < m ; j++)
-		{
-			cin >> a[i][j];
-		}
+		sum += (n / i) * i;
 	}
-
-
-	for(int i = 0; i < n; i++)
-	{
-		for(int j = 0; j < m; j++)
-		{
-			// 특정 육지에서 다른 육지로 가는 최단거리 찾기 && 최대인 최단거리를 갱신하기
-			if (a[i][j] == 'L')
-				bfs(i, j);
-		}
-	}
-
-	cout << mx - 1 << "\n";
-
+	cout << sum << "\n";
 	return 0;
 }
 
