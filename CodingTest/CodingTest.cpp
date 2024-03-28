e5b19014ad6d5f2e60e07b0d74fbafc8ad71c953
MATTTTJ <hoon3022@naver.com>
2024-03-29 02:15:15 +0900

    clear 17425 Type : Math

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -8,17 +8,33 @@
 
 using namespace std;
 
-int n;
-long long sum;
+long long int f[1000001] = { 0, };
+
+long long int g[1000001] = { 0, };
+
+int t, c;
+
 int main()
 {
-	cin >> n;
-
-	for(int i = 1; i <= n; i++)
+	for(int i = 1; i <= 1000000; i++)
 	{
-		sum += (n / i) * i;
+		for(int j = 1; i * j <= 1000000; j++)
+		{
+			// i를 약수로 가지는 수들에게 i를 더해줌
+			g[i * j] += i;
+		}
+		g[i] += g[i - 1];
+		// g(x)를 만들어주는 부분, 이 부분이 없으면 f[x]만 구하는 꼴이다.
 	}
-	cout << sum << "\n";
+
+	scanf("%d", &t);
+
+	while(t--)
+	{
+		scanf("%d", &c);
+
+		printf("%lld\n", g[c]);
+	}
 	return 0;
 }
 
