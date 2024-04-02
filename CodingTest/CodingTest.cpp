ad513309b786aa26a4c3ec313503baa6e0667d98
MATTTTJ <hoon3022@naver.com>
2024-04-02 17:51:47 +0900

    clear 16637 type : 누적합, 브루트포스, 완전 탐색

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -8,27 +8,64 @@
 
 using namespace std;
 
-int n, m, cnt;
-int a[1000001] = { 0,1 };
+vector<int> num;
+vector<int> oper_str;
+int n, ret = -987654321;
+string s;
+
+int oper(char a, int b, int c)
+{
+	if (a == '+') return b + c;
+	if (a == '-') return b - c;
+	if (a == '*') return b * c;
+}
+
+void go(int here, int _num)
+{
+	// 디버깅용
+	//cout << _num << " : " << '\n';
+	// 완전 탐색은 기저사례를 세워야한다. here가 끝이면
+	if(here == num.size() -1)
+	{
+		// 맥스값은 최소값을 구해넣는다.
+		ret = max(ret, _num);
+		return;
+	}
+	// 핵심은 두 가지 경우의 수밖에 없다.
+	go(here + 1, oper(oper_str[here],  _num, num[here + 1]));
+
+	if(here + 2 <= num.size() - 1)
+	{
+		int temp = oper(oper_str[here + 1], num[here + 1], num[here + 2]);
+		go(here + 2, oper(oper_str[here], _num, temp));
+	}
+
+	return;
+}
+
 int main()
 {
-	cin.tie(0);
-	cout.tie(0);
-	cin >> n >> m;
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-	for(int i = 2; i <= m; i++)
+	cin >> n;
+	cin >> s;
+	// 누적합을 그래프로 접근함
+	// 인덱스를 기준으로 나눈다. 
+	for(int i = 0; i < n; i++)
 	{
-		for(int j = 2; i * j <= m; j++)
+		if(i % 2 == 0)
 		{
-			a[i * j] = 1; // 2부터 시작해서 해당 수를 제외한 해당 수의 배수들을 제거한다.
+			num.push_back(s[i] - '0');
+		}
+		else
+		{
+			oper_str.push_back(s[i]);
 		}
 	}
-
-	for(int i = n; i <= m; i++)
-	{
-		if (!a[i])
-			cout << i << "\n";
-	}
+	// 초기값을 기반으로 탐색한다. 
+	go(0, num[0]);
+	cout << ret << "\n";
 
 	return 0;
 }
