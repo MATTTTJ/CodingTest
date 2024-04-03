06527992bc6870ded74b8b3c264754a3a338582f
MATTTTJ <hoon3022@naver.com>
2024-04-03 11:47:10 +0900

    clear 12851 type : BFS

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -7,65 +7,64 @@
 #include <cstring>
 
 using namespace std;
-
-vector<int> num;
-vector<int> oper_str;
-int n, ret = -987654321;
-string s;
-
-int oper(char a, int b, int c)
-{
-	if (a == '+') return b + c;
-	if (a == '-') return b - c;
-	if (a == '*') return b * c;
-}
-
-void go(int here, int _num)
-{
-	// 디버깅용
-	//cout << _num << " : " << '\n';
-	// 완전 탐색은 기저사례를 세워야한다. here가 끝이면
-	if(here == num.size() -1)
-	{
-		// 맥스값은 최소값을 구해넣는다.
-		ret = max(ret, _num);
-		return;
-	}
-	// 핵심은 두 가지 경우의 수밖에 없다.
-	go(here + 1, oper(oper_str[here],  _num, num[here + 1]));
-
-	if(here + 2 <= num.size() - 1)
-	{
-		int temp = oper(oper_str[here + 1], num[here + 1], num[here + 2]);
-		go(here + 2, oper(oper_str[here], _num, temp));
-	}
-
-	return;
-}
+const int MAX = 200000;
+int visited[MAX + 4];
+long long cnt[MAX + 4];
+int n, m;
 
 int main()
 {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> n;
-	cin >> s;
-	// 누적합을 그래프로 접근함
-	// 인덱스를 기준으로 나눈다. 
-	for(int i = 0; i < n; i++)
+	cin >> n >> m;
+
+	if(n==m)
 	{
-		if(i % 2 == 0)
+		// n과 m이 같은 값이 들어올 경우
+		puts("0");
+		puts("1");
+		return 0;
+	}
+
+	visited[n] = 1;
+	cnt[n] = 1;
+	// BFS는 q.push, !q.empty, int now = q.front(), q.pop()만이라도 기억해내자
+	queue<int> q;
+	q.push(n);
+	while(!q.empty())
+	{
+		int now = q.front();
+		q.pop();
+		// -1, +1, *2 의 이동방식이 있다.
+		for(int next : {now -1, now +1, now*2})
 		{
-			num.push_back(s[i] - '0');
-		}
-		else
-		{
-			oper_str.push_back(s[i]);
+			// 오버플로우 체크
+			if( 0 <= next && next <= MAX)
+			{
+				// 방문 안했다면
+				if(!visited[next])
+				{
+					q.push(next);
+					// 가중치 증가
+					visited[next] = visited[now] + 1;
+					// cnt[next] 는 처음 방문하는 곳이라면 0일 것이다.
+					// 이전까지 방문했던 가짓수를 다음 방문하는 곳의 가짓수에 더한다.
+					cnt[next] += cnt[now];
+				}
+				// 다만 최단 거리일 경우에만 가짓수를 추가하도록한다.
+				// 최단 거리가 아닌데 최단거리 가짓수를 추가하면 안됨.
+				else if(visited[next] == visited[now] +1)
+				{
+					cnt[next] += cnt[now];
+				}
+			}
 		}
 	}
-	// 초기값을 기반으로 탐색한다. 
-	go(0, num[0]);
-	cout << ret << "\n";
+	// 시간이 얼마나 걸리는 지 = 최단거리 - 1
+	cout << visited[m] - 1 << '\n';
+	cout << cnt[m] << '\n';
+		
 
 	return 0;
 }
