4c34a6c4021771e4c0d6feb7b97b99663673bc6b
MATTTTJ <hoon3022@naver.com>
2024-04-03 17:43:47 +0900

    clear 13913 type :  BFS

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -7,59 +7,54 @@
 #include <cstring>
 
 using namespace std;
+#define prev aaa
+#define next aaaa
 
-int n;
-int a[1000004];
-bool flag = false;
+const int max_n = 200004;
+int visited[max_n], prev[max_n], n, k, ret, here, cnt, next;
+vector<int> v;
+queue<int> q;
+
 int main()
 {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	for(int i = 2; i <= 1000000; i++)
+	cin >> n >> k;
+	visited[n] = 1;
+	q.push(n);
+	while(!q.empty())
 	{
-		a[i] = i;
-	}
-	for(int i = 2; i * i <= 1000000; i++)
-	{
-		if(a[i] == 0)
-			continue;
-
-		for(int j = i * i; j <= 1000000; j += i)
+		here = q.front();
+		q.pop();
+		if (here == k)
 		{
-			a[j] = 0;
-		}
-	}
-
-	while (true)
-	{
-		cin >> n;
-		if (n == 0)
+			ret = visited[k];
 			break;
-		flag = false;
-		for (int i = 1; i < n; i++)
-		{
-			if (a[i] == 0)
-				continue;
-			if (flag)
-				break;
-
-			for(int i = 3; i <= n; i += 2)
-			{
-				// 에라토스테네스의 체를 이용해 n 의 최대값까지 소수를 구한다.
-				// i는 3부터 시작해 홀수를 만들기 위해 2씩 키워가며, i와 n-i가 소수인지 체크한다. i + n - i = n
-				// 완전 탐색으로 돌면 시간 복잡도가 너무 커지니까 브루트포스로 접근
-				if (a[i] != 0 && a[n - i] != 0)
-				{
-					cout << n << " = " << i << " + " << n - i << "\n";
-					flag = true;
-					break;
-				}
-			}
 		}
-		if (flag == false)
-			cout << "Goldbach's conjecture is Wrong.\n";
+		for (int next : {here - 1, here + 1, here * 2})
+		{
+			if (next >= max_n || next < 0 || visited[next])
+				continue;
+			visited[next] = visited[here] + 1;
+			prev[next] = here;
+			q.push(next);
+		}
 	}
+	// prev 배열 안에 각자 이전의 값이 들어있음
+	for(int i = k ; i != n; i = prev[i])
+	{
+		v.push_back(i);
+	}
+
+	v.push_back(n);
+	// 최단거리 -1이 최소 시간이다. 
+	cout << ret - 1 << '\n';
+	// 방문했던 것 뒤집기
+	reverse(v.begin(), v.end());
+
+	for (int i : v)
+		cout << i << ' ';
 	return 0;
 }
 
