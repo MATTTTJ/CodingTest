12c9d178b108e851daf21c9483c9335f70ed5337
MATTTTJ <hoon3022@naver.com>
2024-04-26 11:09:32 +0900

    clear 1929 type : 소수, 에라토스테네스의 체

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -9,64 +9,32 @@
 
 using namespace std;
 
-const int INF = 987654321;
+int IsPrime[1000001] = {0, 1};
 
-int n, m, h, a, b, ret = INF, visited[34][34];
-bool check()
-{
-	for(int i =1; i <= n; i++)
-	{
-		int start = i;
-		for(int j =  1; j <= h; j++)
-		{
-			if (visited[j][start])
-				start++;
-			else if (visited[j][start - 1])
-				start--;
-		}
-		if (start != i)
-			return false;
-	}
-	return true;
-}
-
-void go(int here, int cnt)
-{
-	if (cnt > 3 || cnt >= ret)
-		return;
-	if(check())
-	{
-		ret = min(ret, cnt);
-		return;
-	}
-	for(int i = here; i <= h; i++)
-	{
-		for(int j = 1; j < n; j++)
-		{
-			if(visited[i][j] || visited[i][j-1] || visited[i][j +1])
-				continue;
-			visited[i][j] = 1;
-			go(i, cnt + 1);
-			visited[i][j] = 0;
-		}
-	}
-}
+int n, m, cnt;
 
 int main()
 {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> n >> m >> h;
-	for(int i =0; i < m; i++)
+	for (int i = 2; i <= 1000000; i++)
 	{
-		cin >> a >> b;
-		visited[a][b] = 1;
+		for (int j = 2; i * j <= 1000000; j++)
+		{
+			IsPrime[i * j] = 1;
+		}
 	}
 
-	go(1, 0);
+	cin >> n >> m;
 
-	cout << ((ret == INF) ? -1 : ret) << "\n";
+	for(int i = n; i <= m; i++)
+	{
+		if(!IsPrime[i])
+		{
+			cout << i << "\n";
+		}
+	}
 
 	return 0;
 }
