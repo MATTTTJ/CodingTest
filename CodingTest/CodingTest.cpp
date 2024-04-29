18937a1805a5c7c6a59b0fbcee0ce35145b9eaff
MATTTTJ <hoon3022@naver.com>
2024-04-29 14:44:57 +0900

    clear 3085 type 브루트포스

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -6,35 +6,91 @@
 #include <map>
 #include <cstring>
 #include <string>
+#include <unordered_map>
+#define MAX 51
 
 using namespace std;
 
-int IsPrime[1000001] = {0, 1};
 
-int n, m, cnt;
+int n, maxCandy;
+char candy[MAX][MAX];
+
+void check()
+{
+	for(int i = 0; i < n; i++)
+	{
+		int count = 1;
+		for(int j = 0; j < n; j++)
+		{
+			if(candy[i][j] == candy[i][j+1])
+			{
+				count++;
+			}
+			else
+			{
+				if (maxCandy < count)
+					maxCandy = count;
+				count = 1;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		int count = 1;
+		for (int j = 0; j < n; j++)
+		{
+			if (candy[j][i] == candy[j+1][i])
+			{
+				count++;
+			}
+			else
+			{
+				if (maxCandy < count)
+					maxCandy = count;
+				count = 1;
+			}
+		}
+	}
+	
+}
 
 int main()
 {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	for (int i = 2; i <= 1000000; i++)
+	cin >> n;
+
+	for(int i = 0; i < n; i++)
 	{
-		for (int j = 2; i * j <= 1000000; j++)
+		for(int j = 0; j < n; j++)
 		{
-			IsPrime[i * j] = 1;
+			cin >> candy[i][j];
 		}
 	}
 
-	cin >> n >> m;
-
-	for(int i = n; i <= m; i++)
+	// 행 단위로 교환해가며 가장 긴 부분을 체크함
+	for(int i = 0; i < n; i++)
 	{
-		if(!IsPrime[i])
+		for(int j = 0; j < n-1; j++)
 		{
-			cout << i << "\n";
+			swap(candy[i][j], candy[i][j + 1]);
+			check();
+			swap(candy[i][j], candy[i][j + 1]);
 		}
 	}
+	// 열 단위로 교환해가며 가장 긴 부분을 체크함
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n - 1; j++)
+		{
+			swap(candy[j][i], candy[j+1][i]);
+			check();
+			swap(candy[j][i], candy[j+1][i]);
+		}
+	}
+	cout << maxCandy << '\n';
 
 	return 0;
 }
