35ace74a5382eab2d95e6bfe37a7da84924a60f3
MATTTTJ <hoon3022@naver.com>
2024-06-18 23:50:07 +0900

    clear 25206 type 수학, 구현, 문자열

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -10,29 +10,51 @@
 
 using namespace std;
 
-vector<string> vec_s = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
-int idx;
-string str;
+vector<pair<string, float>> var = { {"A+", 4.5}, {"A0", 4.0}, {"B+", 3.5}, {"B0", 3.0}, {"C+", 2.5}, {"C0", 2.0}, {"D+", 1.5}, {"D0", 1.0}, {"F", 0}, {"P", 0}};
+
+vector<pair<string, float>> tmp;
+
+string sub, s;
+float n, sum;
+float Allsum;
 
 int main()
 {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> str;
-
-	for(int i = 0; i < vec_s.size(); i++)
+	for(int i = 0; i < 20; i++)
 	{
-		while(true)
-		{
-			idx = (int)str.find(vec_s[i]);
-			if (idx == string::npos)
-				break;
-			str.replace(idx, vec_s[i].length(), "#");
-		}
+		cin >> sub >> n >> s;
+		if(s != "P")
+			sum += n;
+		tmp.push_back({ s, n });
 	}
 
-	cout << str.length();
+	for(auto it : tmp)
+	{
+		// 패스는 통과
+		if(it.first.find("P") != string::npos)
+		{
+			continue;
+		}
+		// 패스가 아닌 경우 학점과 전공 평균을 곱한 뒤, 학점을 전부 더한 값으로 나눈다. 
+		else
+		{
+			float mul = 0;
+			for(auto temp : var)
+			{
+				// 같은 글자 찾음
+				if(mul == 0 && (temp.first.find(it.first) != string::npos))
+				{
+					mul = temp.second;
+					break;
+				}
+			}
+			Allsum += it.second * mul ;
+		}
+	}
+	cout << Allsum / sum;
 
 	return 0;
 }
